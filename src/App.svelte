5e3ecab67813854b499de92aa4503b73d6b768da
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2024-06-11 23:01:30 +0200

    kk ui kit elements can be now tested in this project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import "./app.css";
+  import Block from "./lib/Block.svelte";
+  import MeltCheckbox from "./lib/MeltCheckbox.svelte";
+
+  let t = false;
+</script>
+
+<main class="p-20 bg-black">
+  <Block>
+    <span class="text-white">yooo</span></Block
+  >
+  <MeltCheckbox target={t} title={"whatever"} />
+</main>
