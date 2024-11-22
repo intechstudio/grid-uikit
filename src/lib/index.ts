6060d61cc893ad94f51cfeccf21838fa02a9457a
elsoazemelet <elsoazemelet@gmail.com>
2024-11-22 17:34:14 +0100

    Added MeltCombo and Tree

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -15,3 +15,5 @@ export { default as AtomicInput } from "./AtomicInput.svelte";
 export { default as AtomicSuggestions } from "./AtomicSuggestions.svelte";
 export { contextMenu, contextTarget } from "./context-target";
 export { default as SvgIcon } from "./SvgIcon.svelte";
+export { default as Tree } from "./Tree.svelte";
+export { default as MeltCombo } from "./MeltCombo.svelte";
