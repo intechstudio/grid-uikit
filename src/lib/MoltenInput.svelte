7adef8edec0a6f2d779e5e1534066dc2547eecbe
unknown <elsoazemelet@gmail.com>
2025-03-31 15:35:07 +0200

    Added input and change event for MeltInput

--- src/lib/MoltenInput.svelte
+++ src/lib/MoltenInput.svelte
@@ -1,11 +1,45 @@
 <script lang="ts">
+  import { createEventDispatcher, onMount } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
   export let disabled = false;
-  export let target: any;
+  export let target: string;
+  export let availableCharacters: number | undefined = undefined;
   export function focus() {
     input.focus();
   }
 
   let input: HTMLElement;
+  let oldValue: string | undefined = undefined;
+
+  onMount(() => {
+    oldValue = target;
+  });
+
+  function handleChange(e: any) {
+    dispatch("change", {
+      value: target,
+    });
+  }
+
+  $: handleInput(target);
+
+  function handleInput(value: string) {
+    const diff =
+      typeof availableCharacters === "undefined"
+        ? Infinity
+        : availableCharacters - value.length;
+
+    if (diff >= 0) {
+      oldValue = value;
+      dispatch("input", {
+        value: value,
+      });
+    } else {
+      oldValue = value;
+    }
+  }
 </script>
 
 <input
@@ -13,4 +47,5 @@
   {disabled}
   class="w-full flex px-2 py-2 text-white text-opacity-80 flex-grow bg-black bg-opacity-10 border border-black border-opacity-20 focus:border-green-500 focus:outline-none"
   bind:value={target}
+  on:change={handleChange}
 />
