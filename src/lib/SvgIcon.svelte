7a00bae9d9825c0cf085b125ac70c85f0a17b3d0
gergelybartha <gergely.bartha95@gmail.com>
2026-08-04 12:40:32 +0200

    icons adjust

--- src/lib/SvgIcon.svelte
+++ src/lib/SvgIcon.svelte
@@ -2,14 +2,23 @@
   import iconMap from "./icons";
 
   export let iconPath: string = "";
-  export let width: number = 15;
-  export let height: number = 15;
+  export let width: number = 1.2;
+  export let height: number = 1.2;
   export let fill: string | undefined = undefined;
+  export let iconData: string | undefined = undefined;
+  export let scale: number = 1;
 </script>
 
-<svg style:fill style:width={`${width}px`} style:height={`${height}px`}>
+<svg
+  style:fill
+  style:width={`${width}em`}
+  style:height={`${height}em`}
+  style:transform={`scale(${scale})`}
+>
   <g>
-    {#if iconMap[iconPath]}
+    {#if iconData}
+      {@html iconData}
+    {:else if iconMap[iconPath]}
       {@html iconMap[iconPath]}
     {:else}
       <text x="10" y="20">Icon not found: {iconPath}</text>
